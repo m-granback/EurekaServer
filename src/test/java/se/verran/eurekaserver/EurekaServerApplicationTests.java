eec7b69ed98285c7e8b0016c0c4f4978d9c2cfc2
MichaelGranbäck <michael.granback@outlook.com>
2024-05-21 10:24:48 +0200

    Initial commit

---
+++ src/test/java/se/verran/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.verran.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
